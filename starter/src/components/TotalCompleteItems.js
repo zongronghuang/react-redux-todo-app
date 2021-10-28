6fa3d4e58e668fb9ab961df0ba9bea85cbc75cbf
zongronghuang <zongrong.h886@gmail.com>
2021-10-28 17:07:20 +0800

    feat: initialize project and add addTodo action

--- starter/src/components/TotalCompleteItems.js
+++ starter/src/components/TotalCompleteItems.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React from "react";
 
 const TotalCompleteItems = () => {
-	return <h4 className='mt-3'>Total Complete Items: 5</h4>;
+  return (
+    <h4 className="mt-3">
+      {console.info("[render] TotalCompleteItems")}
+      Total Complete Items: 5
+    </h4>
+  );
 };
 
 export default TotalCompleteItems;
---
+++ starter/src/redux/todosSlice.js
@@ -0,0 +1,33 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const todosSlice = createSlice({
+  name: "todos",
+  initialState: [
+    { id: 1, title: "todo1", completed: false },
+    { id: 2, title: "todo2", completed: false },
+    { id: 3, title: "todo3", completed: true },
+    { id: 4, title: "todo4", completed: false },
+    { id: 5, title: "todo5", completed: false },
+  ],
+  reducers: {
+    addTodo: (state, action) => {
+      console.log("action", action);
+      // {type: 'todos/REDUCER_NAME', payload: {title: ...}}
+
+      const todo = {
+        id: new Date(),
+        title: action.payload.title,
+        completed: false,
+      };
+
+      state.push(todo);
+    },
+  },
+});
+
+console.log("[todosSlice]", todosSlice);
+console.log("[todosSlice.actions]", todosSlice.actions);
+
+export const { addTodo } = todosSlice.actions;
+
+export default todosSlice.reducer;
